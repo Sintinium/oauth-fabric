9d17e92af4420c6bbb16bfa9b0ad05d5abbdb315
LifeIsAParadox <LifeIsAParadox@users.noreply.github.com>
2022-03-09 09:18:09 +0100

    update to 1.18.2

--- src/main/java/com/sintinium/oauthfabric/gui/profile/FakeWorld.java
+++ src/main/java/com/sintinium/oauthfabric/gui/profile/FakeWorld.java
@@ -3,6 +3,7 @@ package com.sintinium.oauthfabric.gui.profile;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.world.Difficulty;
 import net.minecraft.world.World;
 
@@ -16,8 +17,7 @@ public class FakeWorld extends ClientWorld {
         }
         return instance;
     }
-
     public FakeWorld() {
-        super(FakeClientPlayNetHandler.getInstance(), new Properties(Difficulty.EASY, false, true), World.OVERWORLD, FakeDimensionType.getInstance(), 0, 0, () -> null, new WorldRenderer(MinecraftClient.getInstance(), MinecraftClient.getInstance().getBufferBuilders()), false, 0L);
+        super(FakeClientPlayNetHandler.getInstance(), new Properties(Difficulty.EASY, false, true), World.OVERWORLD, new RegistryEntry.Direct<>(FakeDimensionType.getInstance()), 0, 0, () -> null, new WorldRenderer(MinecraftClient.getInstance(), MinecraftClient.getInstance().getBufferBuilders()), false, 0L);
     }
 }
